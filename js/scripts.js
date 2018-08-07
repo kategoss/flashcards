5386d3c06b2c01200db1ee0c87b77088a11cab70
Krystal Foster and Liza Tennis <krystal@>
2018-08-07 16:13:38 -0700

    add styling to flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,36 @@ $(document).ready(function() {
     $(".definition1").toggle();
     });
   $(".clickable2").click(function() {
-    $(".definition2").show();
+    $(".definition2").toggle();
     });
   $(".clickable3").click(function() {
-    $(".definition3").show();
+    $(".definition3").toggle();
     });
   $(".clickable4").click(function() {
-    $(".definition4").show();
+    $(".definition4").toggle();
     });
   $(".clickable5").click(function() {
-    $(".definition5").show();
+    $(".definition5").toggle();
     });
   $(".clickable6").click(function() {
-    $(".definition6").show();
+    $(".definition6").toggle();
     });
   $(".clickable7").click(function() {
-    $(".definition7").show();
+    $(".definition7").toggle();
     });
   $(".clickable8").click(function() {
-    $(".definition8").show();
+    $(".definition8").toggle();
     });
   $(".clickable9").click(function() {
-    $(".definition9").show();
+    $(".definition9").toggle();
     });
   $(".clickable10").click(function() {
-    $(".definition10").show();
+    $(".definition10").toggle();
     });
   $(".clickable11").click(function() {
-    $(".definition11").show();
+    $(".definition11").toggle();
     });
   $(".clickable12").click(function() {
-    $(".definition12").show();
+    $(".definition12").toggle();
     });
   });
