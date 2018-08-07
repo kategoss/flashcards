182e5d266ad60ee5c682f402c8169f6bfbbdff87
Krystal Foster and Liza Tennis <krystal@>
2018-08-07 14:05:49 -0700

    make all flashcards toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".clickable1").click(function() {
-    $(".definition1").show();
+    $(".definition1").toggle();
     });
   $(".clickable2").click(function() {
     $(".definition2").show();
@@ -8,31 +8,31 @@ $(document).ready(function() {
   $(".clickable3").click(function() {
     $(".definition3").show();
     });
-    $(".clickable4").click(function() {
-      $(".definition4").show();
-      });
-    $(".clickable5").click(function() {
-      $(".definition5").show();
-      });
-    $(".clickable6").click(function() {
-      $(".definition6").show();
-      });
-      $(".clickable7").click(function() {
-        $(".definition7").show();
-        });
-      $(".clickable8").click(function() {
-        $(".definition8").show();
-        });
-      $(".clickable9").click(function() {
-        $(".definition9").show();
-        });
-        $(".clickable10").click(function() {
-          $(".definition10").show();
-          });
-        $(".clickable11").click(function() {
-          $(".definition11").show();
-          });
-        $(".clickable12").click(function() {
-          $(".definition12").show();
-          });
+  $(".clickable4").click(function() {
+    $(".definition4").show();
+    });
+  $(".clickable5").click(function() {
+    $(".definition5").show();
+    });
+  $(".clickable6").click(function() {
+    $(".definition6").show();
+    });
+  $(".clickable7").click(function() {
+    $(".definition7").show();
+    });
+  $(".clickable8").click(function() {
+    $(".definition8").show();
+    });
+  $(".clickable9").click(function() {
+    $(".definition9").show();
+    });
+  $(".clickable10").click(function() {
+    $(".definition10").show();
+    });
+  $(".clickable11").click(function() {
+    $(".definition11").show();
+    });
+  $(".clickable12").click(function() {
+    $(".definition12").show();
+    });
   });
