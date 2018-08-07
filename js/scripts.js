4ba95ae9928ae026bd56fd74123eea7ae129306c
Krystal Foster and Liza Tennis <liza@>
2018-08-07 13:54:52 -0700

    link all headers and definitions to js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    $("p").show();
+  $(".clickable1").click(function() {
+    $(".definition1").show();
     });
+  $(".clickable2").click(function() {
+    $(".definition2").show();
+    });
+  $(".clickable3").click(function() {
+    $(".definition3").show();
+    });
+    $(".clickable4").click(function() {
+      $(".definition4").show();
+      });
+    $(".clickable5").click(function() {
+      $(".definition5").show();
+      });
+    $(".clickable6").click(function() {
+      $(".definition6").show();
+      });
+      $(".clickable7").click(function() {
+        $(".definition7").show();
+        });
+      $(".clickable8").click(function() {
+        $(".definition8").show();
+        });
+      $(".clickable9").click(function() {
+        $(".definition9").show();
+        });
+        $(".clickable10").click(function() {
+          $(".definition10").show();
+          });
+        $(".clickable11").click(function() {
+          $(".definition11").show();
+          });
+        $(".clickable12").click(function() {
+          $(".definition12").show();
+          });
   });
